4e6cb30cec896b3c973bd4917786fb30d67ab2bf
Sean Currlin <sean.currlin@gmail.com>
2023-12-20 18:51:31 -0500

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,10 +26,10 @@ const SearchPage = () => {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300 text-gray-800">
-      <h1 className="text-2xl mb-5">Enter your real estate license number for a portfolio sample</h1>
+      <h1 className="text-2xl mb-5">Portfolio Preview</h1>
       <form className="flex flex-col items-center" onSubmit={handleSubmit}>
         <input type="text" name="licenseNumber" placeholder="Enter license number" className="px-4 py-2 mb-4 border border-gray-300 rounded" value={licenseNumber} onChange={(e) => setLicenseNumber(e.target.value)} />
-        <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">Search</button>
+        <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">Submit</button>
       </form>
     </div>
   );
